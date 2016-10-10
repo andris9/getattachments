b6256e48a33f567c6c65d6cd945088677165a5a9
Andris Reinman <andris@kreata.ee>
2016-10-10 13:43:54 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,19 @@
+# getattachments
+
+## Install
+
+```bash
+$ npm install -g getattachments
+```
+
+## Usage
+
+Pipe an email message to `getattachments` command. All files are dumped to current folder. If a file exists, it gets overwritten.
+
+```
+cat message.eml | getattachments
+```
+
+## License
+
+**MIT**
---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+/* eslint no-unused-expressions: 0, global-require: 0, no-console: 0 */
+'use strict';
+
+const mailsplit = require('mailsplit');
+const Splitter = mailsplit.Splitter;
+const Joiner = mailsplit.Joiner;
+const Streamer = mailsplit.Streamer;
+const libmime = require('libmime');
+const path = require('path');
+const fs = require('fs');
+
+let streamer = new Streamer(node => !node.multipart);
+
+let filenames = new Set();
+
+streamer.on('node', data => {
+    let extension = libmime.detectExtension(data.node.contentType || 'application/octet-stream');
+    let filename = path.parse(data.node.filename || (data.node.contentType || 'attachment').split('/').shift() + '.' + extension);
+
+    let base = (filename.name || 'attachment').replace(/[\/\\]/g, '_').replace(/\.+/g, '.').replace(/[\x00-\x1F]/g, '_'); // eslint-disable-line no-control-regex
+    let fname;
+    let i = 0;
+    while (1) { // eslint-disable-line no-constant-condition
+        fname = base + (i ? '-' + i : '') + filename.ext;
+        i++;
+        if (filenames.has(fname)) {
+            continue;
+        }
+        filenames.add(fname);
+        break;
+    }
+
+    data.decoder.pipe(fs.createWriteStream(path.join(process.cwd(), fname)));
+    data.done();
+});
+
+process.stdin.pipe(new Splitter()).pipe(streamer).pipe(new Joiner()).pipe(process.stdout);
